245d7bc90072cbdc9d7fd95823fa9ea3f6219573
Yuriy Kosovskiy <ykosovsky@gmail.com>
2014-04-15 14:53:31 +0300

    Rename interface

---
+++ Tasks/Task1/src/com/kosovskiy/Salary.java
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Module:  Statement.java
+ * Author:  Юрий
+ * Purpose: Defines the Interface Statement
+ ***********************************************************************/
+
+package com.kosovskiy;
+
+import java.util.*;
+
+/** @pdOid 5becd5cd-c07e-4046-a1ce-2fee0b394522 */
+public interface Salary {
+
+    double calSalary(double budget);
+    
+}
