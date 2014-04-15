859161a727b1a05f254ae2ca1734131427cdceff
Yuriy Kosovskiy <ykosovsky@gmail.com>
2014-04-15 12:51:47 +0300

    Add file View.java View model

---
+++ Tasks/Task1/src/com/kosovskiy/View.java
@@ -0,0 +1,13 @@
+/***********************************************************************
+ * Module:  View.java
+ * Author:  Юрий
+ * Purpose: Defines the Class View
+ ***********************************************************************/
+
+package com.kosovskiy;
+
+import java.util.*;
+
+/** @pdOid 1d444f4d-253f-43f6-8e0d-578c731007f3 */
+public class View {
+}
